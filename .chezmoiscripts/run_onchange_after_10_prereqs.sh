d20e1ae1f7d6f1a91f8f80cda8f0edf16fdc2a0d
Clint Olson <clint@populi.co>
2026-02-15 10:37:04 -0800

    Fix up array passing and silence dpkg-query check

--- .chezmoiscripts/run_onchange_after_10_prereqs.sh
+++ .chezmoiscripts/run_onchange_after_10_prereqs.sh
@@ -8,12 +8,12 @@ is_brew_package_installed() {
 
 is_apt_package_installed() {
   local pkg="$1"
-  [ $(dpkg-query -W -f='${Status}' "$pkg") == "install ok installed" ]
+  [ "$(dpkg-query -W -f='${Status}' "$pkg" 2> /dev/null)" == "install ok installed" ]
   return $?
 }
 
 needed_pkgs() {
-  local pkg_list="$1"
+  local pkg_list="$@"
   local absent_pkgs=()
   [[ "$OSTYPE" == "darwin"* ]] && pkgmgr="brew" || pkgmgr="apt"
   for pkg in $pkg_list; do
@@ -33,20 +33,19 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
   brew_pkgs=(bash nano wget less poppler squashfs p7zip pigz)
-  absent_pkgs=$(needed_packages "$brew_pkgs")
+  absent_pkgs=$(needed_packages "${brew_pkgs[@]}")
   if [ -n "$absent_pkgs" ]; then
     echo "Missing brew packages.  To install, run '/opt/homebrew/bin/brew install $absent_pkgs'"
   fi
 
 # On Debian-based systems, install base packages with apt-get
 elif [[ "$OSTYPE" == "linux-gnu"* ]] && hash apt-get &> /dev/null; then
-  echo "Installing prereq packages"
   apt_pkgs=(nano wget curl less catdoc poppler-utils squashfs-tools p7zip pigz)
   # "arm" (i.e. armv7 i.e. not arm64) doesn't have python wheels for a couple of packages, so get prereqs
   if [[ "$(uname -m)" == "arm"* ]]; then
     apt_pkgs+=(libffi-dev libsodium-dev)
   fi
-  absent_pkgs=$(needed_pkgs "$apt_pkgs")
+  absent_pkgs=$(needed_pkgs "${apt_pkgs[@]}")
   if [ -n "$absent_pkgs" ]; then
     echo "Missing apt packages.  To install, run 'sudo apt-get install $absent_pkgs'"
   fi
